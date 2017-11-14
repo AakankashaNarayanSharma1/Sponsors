f71cb07d175cc6acaef00407121f242f6a28cf46
AakankashaNarayanSharma <akkutbgs3x@gmail.com>
2017-11-14 19:12:04 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './napp';
-import sponsors from './sponsors';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App data={sponsors} />, document.getElementById('root'));
-registerServiceWorker();
